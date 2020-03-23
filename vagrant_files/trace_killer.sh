88e591a7875a1cac0e33602e8a3d49b0e3d60cf7
Susanna Schwarzmann <susanna@inet.tu-berlin.de>
2020-03-23 22:00:58 +0100

    added trace killer script

---
+++ vagrant_files/trace_killer.sh
@@ -0,0 +1,22 @@
+#check if any old trace-script is running on netem 
+process_id=$(ps aux | grep netem_start_trace)
+to_kill=$(echo $process_id | awk '{print $2}')
+if [[ !  -z  $to_kill  ]];then
+        kill -9 $to_kill
+        echo '-- Killed old trace script---'
+fi
+
+process_id=$(ps aux | grep netem_start_trace)
+to_kill=$(echo $process_id | awk '{print $2}')
+if [[ !  -z  $to_kill  ]];then
+        kill -9 $to_kill
+        echo '-- Killed old trace script---'
+fi
+
+process_id=$(ps aux | grep netem_start_trace)
+to_kill=$(echo $process_id | awk '{print $2}')
+if [[ !  -z  $to_kill  ]];then
+        kill -9 $to_kill
+        echo '-- Killed old trace script---'
+fi
+
