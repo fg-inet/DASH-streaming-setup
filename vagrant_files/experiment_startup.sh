bf154596c7555fc3872cab97a62139ea68ba6e6b
Susanna Schwarzmann <susanna.schwarzmann@inet.tu-berlin.de>
2020-03-27 19:05:25 +0100

    adaptations on automation script

--- vagrant_files/experiment_startup.sh
+++ vagrant_files/experiment_startup.sh
@@ -1,33 +1,34 @@
-#We do three runs with exact same config (trace type, trace num, trace sp)
 browserDir='/home/vagrant/BrowserDir'
 host='192.167.101.13'
-
-for i in {1..1}
+#specify the number of runs to be performed per measurement
+num_runs = 3
+vid_timeout = '1500s'
+for i in {1..$num_runs}
 do
-	#Tears of Steel
 	videoDirNA='CBR_TOS_NA_10/playlist.mpd'
 	videoDirVAR='CBR_TOS_VAR_10/playlist.mpd'
 	trace_folder='tos_trace'
-
+	#itreate over all traces in the specified trace folder
 	traces=( $(ls trace_files/$trace_folder) )
 	for tr in "${traces[@]}"
 	do 
  		echo $tr
 		run_var="trace_"$tr"_run"$i
-		# let the netem begin! 
-		# do it all for VAR video 
+		#measurements for the video variably encoded 
 		vagrant ssh netem -- -t bash trace_killer.sh 
 		sleep 1
-		vagrant ssh netem -- -t timeout 1500s bash netem_start_trace.sh trace_files/$trace_folder/$tr &
+		#start the new trace
+		vagrant ssh netem -- -t timeout $vid_timeout bash netem_start_trace.sh trace_files/$trace_folder/$tr &
 		sleep 1
-		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout 1500s npm start $browserDir $run_var $videoDirVAR $host)"
+		#start the measurement at the client
+		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout $vid_timeout npm start $browserDir $run_var $videoDirVAR $host)"
 		sleep 5
 		# do the same for NA video 
 		vagrant ssh netem -- -t bash trace_killer.sh 
 		sleep 1
-		vagrant ssh netem -- -t timeout 1500s bash netem_start_trace.sh trace_files/$trace_folder/$tr &
+		vagrant ssh netem -- -t timeout $vid_timeout bash netem_start_trace.sh trace_files/$trace_folder/$tr &
 		sleep 1
-		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout 1500s npm start $browserDir $run_var $videoDirNA $host)"
+		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout $vid_timeout npm start $browserDir $run_var $videoDirNA $host)"
 		sleep 5
 
 	done
@@ -44,17 +45,17 @@ do
 		#kill old trace sricpts 
 		vagrant ssh netem -- -t bash trace_killer.sh 
 		sleep 1
-		vagrant ssh netem -- -t timeout 15000s bash netem_start_trace.sh trace_files/$trace_folder/$tr &
+		vagrant ssh netem -- -t timeout $vid_timeout bash netem_start_trace.sh trace_files/$trace_folder/$tr &
 		sleep 1
 		#npm command needs to run in in the fetcher folder!
-		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout 1500s npm start $browserDir $run_var $videoDirVAR $host)"
+		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout $vid_timeout npm start $browserDir $run_var $videoDirVAR $host)"
 		sleep 5
 		# do the same for NA video 
 		vagrant ssh netem -- -t bash trace_killer.sh 
 		sleep 1
-		vagrant ssh netem -- -t timeout 1500s bash netem_start_trace.sh trace_files/$trace_folder/$tr &
+		vagrant ssh netem -- -t timeout $vid_timeout bash netem_start_trace.sh trace_files/$trace_folder/$tr &
 		sleep 1
-		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout 1500s npm start $browserDir $run_var $videoDirNA $host)"
+		vagrant ssh client -- -t "(cd /home/vagrant/DASH-setup/client && timeout $vid_timeout npm start $browserDir $run_var $videoDirNA $host)"
 		sleep 5
 
 	done  
