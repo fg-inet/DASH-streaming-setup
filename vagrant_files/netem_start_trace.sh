4128a003637906c9a202cdfa0100d0cc648cca6e
Susanna Schwarzmann <susanna@inet.tu-berlin.de>
2020-03-27 11:29:33 +0100

    added start trace script

---
+++ vagrant_files/netem_start_trace.sh
@@ -0,0 +1,15 @@
+bandwidth_trace=$1
+
+readarray -t bw_vals < $bandwidth_trace
+
+sleep 1
+
+sudo tc qdisc del dev eth2 root
+sudo tc qdisc add dev eth2 root handle 1: htb default 1
+sudo tc class add dev eth2 parent 1: classid 1:1 htb rate 1000kbit
+
+for line in "${bw_vals[@]}"; do
+	sudo tc class change dev eth2 parent 1: classid 1:1 htb rate "$line"kbit
+	#echo $line    
+	sleep 1
+done
