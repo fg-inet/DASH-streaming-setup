a285bc0825dbd1998cd0b379a51be223edb815fc
Christian Sieber <c.sieber@tum.de>
2020-03-29 19:14:31 +0200

    Fixed Vagrantfile setup and startup configuration.

--- vagrant_files/setup_client.sh
+++ vagrant_files/setup_client.sh
@@ -2,13 +2,6 @@
 # shellcheck disable=SC1068
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-# Put here the IP of the interface connected to the shaping/netem host.
-TEST_IF_IP="192.167.100.11"
-
-TEST_ETH=$( ifconfig | sed -n "/addr:$TEST_IF_IP/{g;H;p};H;x" | awk '{print $1}' )
-
-ip route add 192.167.101.0/24 via 192.167.100.12 dev $TEST_ETH
-
 sudo apt update
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
--- vagrant_files/setup_server.sh
+++ vagrant_files/setup_server.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-# Put here the IP of the interfaces connected to the server and client
-TEST_IF_IP="192.167.101.13"
-
-TEST_ETH=$( ifconfig | sed -n "/addr:$TEST_IF_IP/{g;H;p};H;x" | awk '{print $1}' ) 
-
-ip route add 192.167.100.0/24 via 192.167.101.12 dev $TEST_ETH
-
 sudo apt update
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -18,6 +11,3 @@ cd DASH-setup-local
 
 npm install
 sudo npm install -g --force nodemon
-npm start 
-
-
