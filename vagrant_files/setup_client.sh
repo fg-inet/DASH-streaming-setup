c82b29e450cad23b87766a44dea1f8f196b81f32
Polynomialdivision <vincent@systemli.org>
2020-03-30 12:36:03 +0200

    fix several issues with vagrant and node.js
    Fixed permissions error for directory that prevents
    to install several npm packages and write logs.
    
    Update npm dependencies to fix vulnerabilies and fix
    npm warnings.
    
    Remove unused dependencies in package.json files.
    
    Switch from apt to apt-get. Fixes vagrant warnings.
    
    Add DEBIAN_FRONTEND=noninteractive to apt-get calls to remove
    warning.

--- vagrant_files/setup_client.sh
+++ vagrant_files/setup_client.sh
@@ -2,21 +2,20 @@
 # shellcheck disable=SC1068
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt update
-sudo apt install -y google-chrome-stable
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq google-chrome-stable
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq nodejs
 
 cd /home/vagrant/
 cp -r DASH-setup DASH-setup-local
+chown -R vagrant:vagrant DASH-setup-local
+
 cd DASH-setup-local
 
 npm install
-sudo npm install -g --force nodemon
-
-sudo apt-get install -y xauth
--- vagrant_files/setup_server.sh
+++ vagrant_files/setup_server.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 trap 'echo "# $BASH_COMMAND"' DEBUG
 
-sudo apt update
+sudo DEBIAN_FRONTEND=noninteractive apt-get update
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt install -y nodejs
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq nodejs
+
 cd /home/vagrant/
 cp -r DASH-setup DASH-setup-local
-cd DASH-setup-local
+chown -R vagrant:vagrant DASH-setup-local
 
+cd DASH-setup-local
 npm install
-sudo npm install -g --force nodemon
