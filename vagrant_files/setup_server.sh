155ceaaabce77dc103f24cd493c486d14c211e42
Polynomialdivision <vincent@systemli.org>
2020-03-30 20:44:14 +0200

    fix server log sync

--- vagrant_files/setup_server.sh
+++ vagrant_files/setup_server.sh
@@ -8,6 +8,11 @@ sudo DEBIAN_FRONTEND=noninteractive apt-get install -yq nodejs
 
 cd /home/vagrant/
 cp -r DASH-setup DASH-setup-local
+
+mkdir -p /home/vagrant/DASH-setup/logs/
+rm -r /home/vagrant/DASH-setup-local/logs
+ln -s /home/vagrant/DASH-setup/logs/ /home/vagrant/DASH-setup-local/logs
+
 chown -R vagrant:vagrant DASH-setup-local
 
 cd DASH-setup-local
